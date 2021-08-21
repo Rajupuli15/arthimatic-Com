1a835c4e1c4e96e1a4ee2140d83873e74f6ccf03
rajupuli15 <rajupuli15@gmail.com>
2021-08-21 04:47:05 -0400

    arithmatic-Computation

---
+++ arthimatic-Com7.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x 
+
+read -p "Enter the value of a : : " a
+read -p "Enter the value of b : : " b
+read -p "Enter the value of c : : " c
+
+declare -A results
+results[one]
+results[two]
+results[three]
+results[four]
+
+Array
+m=0
+array[m++]
+array[m++]
+array[m++]
+array[m++]
+
+echo "outcome" ${array[@]}
+
+
+
+
+
